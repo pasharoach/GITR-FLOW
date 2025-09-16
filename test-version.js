6ad884675c8e1b6edb9aab39e8c29e97b710d912
Pavel Platonov <tarakanov@me.com>
2025-09-16 20:44:39 +0300

    8.0.0

--- test-version.js
+++
@@ -1,6 +0,0 @@
-// Тестовый скрипт для проверки версии
-const packageJson = require('./package.json');
-console.log('Версия в package.json:', packageJson.version);
-
-const { app } = require('electron');
-console.log('Версия приложения:', app.getVersion());
